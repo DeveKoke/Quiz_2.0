32e094fbf237a33321bdba849f2e23613717441a
DeveKoke <jorge.be86@gmail.com>
2023-05-21 12:35:23 +0200

    main.html_created

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let arr [];
+let randomNum = Math.floor(Math.random()*20);
+if(!arr.includes(randomNum)){
+    arr.push(randomNum)
+}else{
+    return randomNum
+}
+console.log(randomNum);
+
+for (let i = 0; arr.index(); i++) {
+    const element = array[index];
+    
+}
