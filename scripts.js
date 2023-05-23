f0cffafc7d9956011532ba3335b97369676e5b30
DeveKoke <jorge.be86@gmail.com>
2023-05-23 13:13:40 +0200

    fetch_to_appi_created_and_link_from_index.html_to_main-html

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-let arr [];
-let randomNum = Math.floor(Math.random()*20);
-if(!arr.includes(randomNum)){
-    arr.push(randomNum)
-}else{
-    return randomNum
-}
-console.log(randomNum);
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function() {
+    window.location.href = 'main.html';
+  });
+  
 
-for (let i = 0; arr.index(); i++) {
-    const element = array[index];
-    
+
+
+async function getRandomQuestions() {
+    let randomQuestions = [];
+    const difficulty = document.getElementById('difficulty').value   //*  input user para elegir dificultad.
+
+    try{
+        let response = await fetch(`https://opentdb.com/api.php?amount=50&category=23&difficulty=${difficulty}&type=multiple`);
+        let data = await response.json();
+        let allQuestions = data.results;
+
+        let maxNum = document.getElementById('userNum').value;  //* input number de user
+        let minNum = 5;
+        while (randomQuestions.length != minNum){
+        let num = Math.floor(Math.random()*maxNum);
+            if (!randomQuestions.includes(num)){
+            randomQuestions.push(allQuestions[num]);
+            }
+        }
+    }
+    catch(error){
+        console.log(`ERROR`)
+    }
 }
